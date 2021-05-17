acea76fa803302d296ee08cd606b7f11c3ea3562
MartinVald2170 <noways2170@gmail.com>
2021-05-18 09:16:51 +1000

    search and table added

--- src/Components/Listing.jsx
+++ src/Components/Listing.jsx
@@ -3,8 +3,25 @@ import axios from "axios";
 import SearchIcon from '@material-ui/icons/Search';
 import { useEffect, useState } from "react";
 import {TextField} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
 
 const Listing = () => {
+    const classes = useStyles();
     const [listing, setListing] = useState([])
     const [search, setSearch] = useState("") 
 
@@ -35,17 +52,71 @@ const Listing = () => {
             }} 
             
             />
+            
+
+{/* const columns = [
+  {field: "clientID", hide: true},
+  {field: 'clientName', headerName: "Client Name", width: 160}, 
+  {field: 'clientEmail', headerName: "Email", width: 200},
+  {field: 'clientWorkPhone', headerName: "Phone", width: 130},
+  {field: 'clientIndustry', headerName: "Industry", width: 130},
+  {field: 'clientPocName', headerName: "Point of Contact", width: 160},
+  {field: 'clientWebsite', headerName: "Website", width: 160}
+
+] */}
+
+<TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow >
+            <TableCell>Client Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Phone</TableCell>
+            <TableCell>Industry</TableCell>
+            <TableCell>Point of Contact</TableCell>
+            <TableCell>Website</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+
             {listing.filter(item => {
-                if (search == "") {
-                    return item;
-                }
-                else if (item.clientName.toLowerCase().includes(search.toLowerCase())){
+               if (item.clientName.toLowerCase().includes(search.toLowerCase())){
                     return item
                 }
             }).
             map((item)=> {
-                return (<p>{item.clientName}</p>);
-            })}
+                return (
+           
+            <TableRow key={listing.clientID}
+            >
+              <TableCell component="th" scope="row">
+             {item.clientName} 
+              </TableCell>
+              <TableCell align="right">{item.clientEmail}</TableCell>
+              <TableCell component="th" scope="row">
+             {item.clientWorkPhone} 
+              </TableCell>
+              <TableCell component="th" scope="row">
+             {item.clientIndustry} 
+              </TableCell>
+              <TableCell component="th" scope="row">
+             {item.clientPocName} 
+              </TableCell>
+              <TableCell component="th" scope="row">
+             {item.clientWebsite} 
+              </TableCell>
+   
+            </TableRow>
+         ); })} 
+        </TableBody>
+      </Table>
+    </TableContainer>
+   
+
+
+
+
+
         </div>
     );
 };
